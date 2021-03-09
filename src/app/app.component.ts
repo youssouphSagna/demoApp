039f0da11779a68ee86518db9a91e641873ab392
youssouphSagna <youssouphsagna54@gmail.com>
2021-03-09 20:58:15 +0100

    add user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import * as uuid from 'uuid';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demoApp';
+  users = [];
+  firstname: string;
+  lastname: string;
+  headers = new HttpHeaders({
+    Accept: '*/*'
+  });
+  userForm = this.formBuilder.group({
+    firstname: '',
+    lastname: ''
+  });
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.http.get('https://j5fucbk7qg.execute-api.eu-west-2.amazonaws.com/get', {headers: this.headers}).subscribe(data => {
+      this.users = JSON.parse(JSON.stringify(data));
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.userForm.value);
+    const body = {
+      id: uuid.v4(),
+      firstname: this.userForm.value.firstname,
+      lastname: this.userForm.value.firstname
+    };
+    this.http.post('https://j5fucbk7qg.execute-api.eu-west-2.amazonaws.com/post', body, {headers: this.headers}).subscribe(() => {
+      this.getUsers();
+      this.userForm.reset();
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AppComponent} from './app.component';
+import {MatFormFieldModule} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
